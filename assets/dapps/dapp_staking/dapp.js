714725498ec1b2637e2b0f9648826648bf9e767f
pharaon0x <115258838+pharaon0x@users.noreply.github.com>
2022-10-19 08:49:22 +0300

    Add files via upload

---
+++ assets/dapps/dapp_staking/dapp.js
@@ -0,0 +1,81 @@
+const Web3 = window.Web3
+const Web3Modal = window.Web3Modal.default
+const WalletConnectProvider = window.WalletConnectProvider.default
+const evmChains = window.evmChains
+
+const PROVIDER_OPTIONS = {
+    walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+            rpc: RPC,
+            chainId: CHAIN_ID
+        }
+    }
+}
+
+const W3_MODAL = new Web3Modal({
+    cacheProvider: false,
+    providerOptions: PROVIDER_OPTIONS
+})
+
+var WALLET_ADDRESS = NaN
+var PROVIDER = NaN
+
+var BUTTON_CONNECT = NaN
+var LINK_REF = NaN
+var INPUT_DEPOSIT_DURATION = NaN
+var INPUT_DEPOSIT_AMOUNT = NaN
+
+function get_short_address(address, tailsLength = 5) {
+    return address.substring(0, tailsLength) + '...' + address.substring(address.length - tailsLength, address.length)
+}
+
+function set_wallet_address() {
+    WALLET_ADDRESS = PROVIDER.selectedAddress
+    BUTTON_CONNECT.text(get_short_address(WALLET_ADDRESS))
+}
+
+async function wallet_connect() {
+    setTimeout(() => {
+        BUTTON_CONNECT.blur()
+    }, 500)
+
+    if (await check_wallet_connect()) {
+        return true
+    }
+
+    console.log('Opening a dialog', W3_MODAL)
+    
+    try {
+        PROVIDER = await W3_MODAL.connect()
+        set_wallet_address()
+
+        return true
+    } catch (e) {
+        return false
+    }
+}
+
+async function check_wallet_connect() {
+    try {
+        PROVIDER = web3.currentProvider
+    } catch (e) {
+        console.log('Could find web3 provider', e)
+        return false
+    }
+
+    if (typeof (PROVIDER.selectedAddress) == 'string') {
+        WALLET_ADDRESS = PROVIDER.selectedAddress
+        set_wallet_address()
+        return true
+    } else {
+        return false
+    }
+}
+
+$(function () {
+    BUTTON_CONNECT = $('.btn-connect')
+
+    BUTTON_CONNECT.on('click', wallet_connect)
+})
+
---
+++ assets/dapps/dapp_staking/dapp_setting.js
@@ -0,0 +1,8 @@
+const CONTRACT_ADDRESS = '0x648aB5524a517c43aA39699d8a05FaDFdFC82708'
+
+const CHAIN_ID = 97
+const RPC = {
+    97: 'https://data-seed-prebsc-1-s1.binance.org:8545'
+}
+
+const BLACKLIST = []
